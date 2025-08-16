009b62b6100962e4b79cc83ab0212efeeb892cc5
Artem <154357915+ArtemAvdeevRus@users.noreply.github.com>
2025-08-17 01:37:37 +0300

    Кнопки навигации

--- script.js
+++ script.js
@@ -1,9 +1,45 @@
- console.log ('Скрипт работает');
+
   const video=document.getElementById('videoFon');
-  console.log (video);
   video.addEventListener('play', () => {
-    console.log (document.getElementById('content').className);
-    document.getElementById('content').className +='show'
-    console.log ('Стиль применился');
-    console.log (document.getElementById('content').className);
+  document.getElementById('content').className +='show'
+  });
+  const button_about=document.getElementById('about_btn')
+  button_about.addEventListener('click', function close () {
+    document.getElementById('abaut').style.display='flex';
+    document.getElementById('branch').style.display='none';
+    document.getElementById('coach').style.display='none';
+    document.getElementById('news').style.display='none';
+    document.getElementById('gallery').style.display='none';
+  });
+  const button_branch=document.getElementById('branch_btn')
+  button_branch.addEventListener('click', function close () {
+    document.getElementById('abaut').style.display='none';
+    document.getElementById('branch').style.display='flex';
+    document.getElementById('coach').style.display='none';
+    document.getElementById('news').style.display='none';
+    document.getElementById('gallery').style.display='none';
+  });
+   const button_coach=document.getElementById('coach_btn')
+  button_coach.addEventListener('click', function close () {
+    document.getElementById('abaut').style.display='none';
+    document.getElementById('branch').style.display='none';
+    document.getElementById('coach').style.display='flex';
+    document.getElementById('news').style.display='none';
+    document.getElementById('gallery').style.display='none';
+  });
+   const button_news=document.getElementById('news_btn')
+  button_news.addEventListener('click', function close () {
+    document.getElementById('abaut').style.display='none';
+    document.getElementById('branch').style.display='none';
+    document.getElementById('coach').style.display='none';
+    document.getElementById('news').style.display='flex';
+    document.getElementById('gallery').style.display='none';
+  });
+   const button_gallery=document.getElementById('gallery_btn')
+  button_gallery.addEventListener('click', function close () {
+    document.getElementById('abaut').style.display='none';
+    document.getElementById('branch').style.display='none';
+    document.getElementById('coach').style.display='none';
+    document.getElementById('news').style.display='none';
+    document.getElementById('gallery').style.display='flex';
   });
